d6f29ebb376bd3bcb1e16c0a91a9c33b1a6311d9
Aythami Sanchez Garcia <aythami.1914@gmail.com>
2023-11-15 11:56:38 +0000

    Read more buttom and css changes

--- js/read-more.js
+++ js/read-more.js
@@ -1,30 +1,11 @@
 let hideTextButtoms = document.getElementsByClassName('btn btn-primary read-more-btns');
 
-console.log("hola holita 1")
-console.log(hideTextButtoms);
-
-// let hideText = document.getElementById('hide-text');
-
 let hideTexts = document.getElementsByClassName("hide hide-texts");
 
-console.log("hola holita")
-console.log(hideTexts);
-
 for (let i = 0; i < hideTexts.length; i++) {
-  hideTextButtoms[i].addEventListener('click', function() { toggleText(hideTexts[i]) });
+  hideTextButtoms[i].addEventListener('click', function () { toggleText(hideTexts[i]); });
 }
 
-
-
 function toggleText(elementToToggle) {
-  console.log("holota")
-  console.log(elementToToggle)
   elementToToggle.classList.toggle('show');
-
-  // if(elementToToggle.classList.contains('show')) {
-  // //   hideTextButtom.innerHTML = 'Read Less';
-  // // }
-  // else{
-  //   hideTextButtom.innerHTML = 'Read More';
-  // }
 }
